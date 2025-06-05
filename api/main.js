ae9be04a745b83a30d449a5e7d7df012f9aca0e7
Star Force <cpstarforce@gmail.com>
2025-06-05 19:45:59 +0000

    Update API

--- api/main.js
+++ api/main.js
@@ -1,13 +1,37 @@
 const express = require('express');
+const fs = require('fs');
 const router = express.Router();
+const path = require('path');
+const rawKeys = fs.readFileSync(path.join(__dirname, 'keys.json'), 'utf-8');
+const keys = JSON.parse(rawKeys);
+
+function verify(key) {
+  return keys.includes(key);
+}
+
+router.use(async (req, res, next) => {
+  const apiKey = req.query.key;
+  if (!apiKey) {
+    return res.status(403).json({
+      message: "API key not provided in query"
+    });
+  }
+  if (!verify(apiKey)) {
+    return res.status(403).json({
+      message: "Invalid API key"
+    });
+  }
+  next();
+});
 
 router.get('/ping', (req, res) => {
-  const start = Date.now();
+  const start = req.header("start") || req.query.start || Date.now();
   setImmediate(() => {
     const ping = Date.now() - start;
     res.json({
       message: 'Pong!',
-      ping: ping
+      ping: ping,
+      start: start
     });
   });
 });
